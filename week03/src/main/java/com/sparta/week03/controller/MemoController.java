d70f6b52f17730042563f2bf34d74e6c44dab0a6
Nayujin <00nyj@sookmyung.ac.kr>
2022-09-29 19:35:41 +0900

    [week03] put API 퀴즈

--- week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -16,6 +16,20 @@ public class MemoController {
     private final MemoRepository memoRepository;
     private final MemoService memoService;
 
+    //유진 퀴즈 풀어볼라고 한 흔적 .. 하지만 영 빗나갔다 우핫핫 정답은 바로 아래에 ~~
+//    @PutMapping("/api/{id}")
+//    public Long updateMemo(@PathVariable Long id,@RequestBody MemoRequestDto requestDto){
+//        memoRepository.findById(id);
+//        Memo memo = new Memo(requestDto);
+//        memo.update(requestDto);
+//        return id;
+//    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        memoService.update(id, requestDto);
+        return id;
+    }
     @PostMapping("/api/memos")
     public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
