94eb358411bab61de3bfa37c36c6e0459ee06009
Nayujin <00nyj@sookmyung.ac.kr>
2022-09-29 19:22:17 +0900

    [week03] Controller 만들기

---
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -0,0 +1,35 @@
+package com.sparta.week03.controller;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.models.MemoRequestDto;
+import com.sparta.week03.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
+    }
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        memoRepository.deleteById(id);
+        return id;
+    }
+}
