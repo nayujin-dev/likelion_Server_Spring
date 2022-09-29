ab3fa79c85d3ab3f45efcdfb172ccb4d2291c398
Nayujin <00nyj@sookmyung.ac.kr>
2022-09-29 20:58:29 +0900

    [week03] 과제: 조회 시간 24시로 제한하기

--- week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -7,14 +7,9 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@RequiredArgsConstructor
-@RestController
-public class MemoController {
-
-    private final MemoRepository memoRepository;
-    private final MemoService memoService;
 
     //유진 퀴즈 풀어볼라고 한 흔적 .. 하지만 영 빗나갔다 우핫핫 정답은 바로 아래에 ~~
 //    @PutMapping("/api/{id}")
@@ -24,26 +19,35 @@ public class MemoController {
 //        memo.update(requestDto);
 //        return id;
 //    }
+    @RequiredArgsConstructor
+    @RestController
+    public class MemoController {
 
-    @PutMapping("/api/memos/{id}")
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        memoService.update(id, requestDto);
-        return id;
-    }
-    @PostMapping("/api/memos")
-    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
-        Memo memo = new Memo(requestDto);
-        return memoRepository.save(memo);
-    }
+        private final MemoRepository memoRepository;
+        private final MemoService memoService;
 
-    @GetMapping("/api/memos")
-    public List<Memo> getMemos() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
-    }
+        @GetMapping("/api/memos")
+        public List<Memo> getMemos() {
+            LocalDateTime start = LocalDateTime.now().minusDays(1);
+            LocalDateTime end = LocalDateTime.now();
+            return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start, end);
+        }
 
-    @DeleteMapping("/api/memos/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
-        memoRepository.deleteById(id);
-        return id;
+        @PostMapping("/api/memos")
+        public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+            Memo memo = new Memo(requestDto);
+            return memoRepository.save(memo);
+        }
+
+        @PutMapping("/api/memos/{id}")
+        public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+            memoService.update(id, requestDto);
+            return id;
+        }
+
+        @DeleteMapping("/api/memos/{id}")
+        public Long deleteMemo(@PathVariable Long id) {
+            memoRepository.deleteById(id);
+            return id;
+        }
     }
-}
