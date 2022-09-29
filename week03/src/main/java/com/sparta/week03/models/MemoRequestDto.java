0e1409fa55ccd8cd257121f11825bad411aee97d
Nayujin <00nyj@sookmyung.ac.kr>
2022-09-29 20:56:43 +0900

    [week03] 클라이언트 1차 구현

--- week03/src/main/java/com/sparta/week03/models/MemoRequestDto.java
+++ week03/src/main/java/com/sparta/week03/models/MemoRequestDto.java
@@ -10,8 +10,5 @@ public class MemoRequestDto {
     private String username;
     private String contents;
 
-    public MemoRequestDto(String username, String contents) {
-        this.username = username;
-        this.contents = contents;
-    }
+
 }
