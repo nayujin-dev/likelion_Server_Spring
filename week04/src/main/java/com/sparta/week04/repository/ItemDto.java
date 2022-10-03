47be7a304930c74012dc61e594861053406f5d4b
Nayujin <00nyj@sookmyung.ac.kr>
2022-10-03 14:35:24 +0900

    [week04] 키워드로 상품 검색하기 1

--- week04/src/main/java/com/sparta/week04/repository/ItemDto.java
+++ week04/src/main/java/com/sparta/week04/repository/ItemDto.java
@@ -1,4 +1,20 @@
 package com.sparta.week04.repository;
 
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import lombok.Getter;
+import org.json.JSONObject;
+
+@Getter
 public class ItemDto {
+    private String title;
+    private String link;
+    private String image;
+    private int lprice;
+
+    public ItemDto(JSONObject itemJson) {
+        this.title = itemJson.getString("title");
+        this.link = itemJson.getString("link");
+        this.image = itemJson.getString("image");
+        this.lprice = itemJson.getInt("lprice");
+    }
 }
--- week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,27 +1,49 @@
 package com.sparta.week04.utils;
 
+import com.fasterxml.jackson.databind.util.JSONWrappedObject;
+import com.sparta.week04.repository.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 public class NaverShopSearch {
-    public String search(){
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", "Oos56uK8zuCXm0aVUfES");
-        headers.add("X-Naver-Client-Secret", "CUGeTfozxY");
+        headers.add("X-Naver-Client-Id", "zdqMoIkFaK8uKvC2oNY2");
+        headers.add("X-Naver-Client-Secret", "LiZfsgtuD5");
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=computer", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
         System.out.println("Response status: " + status);
         System.out.println(response);
+
         return response;
     }
-    public static void main(String[] args){
+
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items  = rjson.getJSONArray("items");
+        List<ItemDto> ret = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            System.out.println(itemJson);
+            ItemDto itemDto = new ItemDto(itemJson);
+            ret.add(itemDto);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
         NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+        String ret = naverShopSearch.search("아이맥");
+        naverShopSearch.fromJSONtoItems(ret);
     }
 }
